8d63b3ff0b921af8ef5fdf8865eeb8c8aaf14ec2
vpvnguyen <vpvnguyen@gmail.com>
2020-12-26 09:28:40 -0800

    import Kent C Dodd's useContext example

--- src/App.js
+++ src/App.js
@@ -3,12 +3,41 @@ import "./App.css";
 
 import LifecycleMethods from "./components/LifecycleMethods.jsx";
 import Hooks from "./components/Hooks.jsx";
+import {
+  CountProvider,
+  CountDisplay,
+  Counter,
+} from "./components/UseContext/KentCDodds/CountContext";
+
+const style = {
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    margin: "0 auto",
+  },
+  container: {
+    margin: "1rem",
+    padding: "1rem",
+    border: "1px solid black",
+  },
+};
 
 function App() {
   return (
-    <div>
-      <LifecycleMethods />
-      <Hooks />
+    <div style={style.root}>
+      <div style={style.container}>
+        <LifecycleMethods />
+      </div>
+      <div style={style.container}>
+        <Hooks />
+      </div>
+
+      <div style={style.container}>
+        <CountProvider>
+          <CountDisplay />
+          <Counter />
+        </CountProvider>
+      </div>
     </div>
   );
 }
