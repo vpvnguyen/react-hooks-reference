25359b2968db42e8045efbe3609a99bc0574964e
vpvnguyen <vpvnguyen@gmail.com>
2021-01-18 14:12:28 -0800

    create PropDrilling example using useContext

---
+++ src/components/UseContext/PropDrilling/PropDrilling.js
@@ -0,0 +1,74 @@
+import React, { useState, createContext, useContext } from "react";
+import { style } from "../../../App";
+
+const MyContext = createContext();
+
+const ParentComponent = () => {
+  const [state, setState] = useState("Parent State");
+
+  // create an object containing both the value and the setter
+  const contextValue = { state, setState };
+
+  return (
+    <div style={style.container}>
+      <h3>Parent</h3>
+      <p>Context: {state}</p>
+      <button onClick={() => setState("Parent State")}>Parent setState</button>
+      <MyContext.Provider value={contextValue}>
+        <ChildOneComponent />
+        <ChildTwoComponent />
+      </MyContext.Provider>
+    </div>
+  );
+};
+
+const ChildOneComponent = () => {
+  const { state, setState } = useContext(MyContext);
+
+  return (
+    <div style={style.container}>
+      <h3>Child 1</h3>
+      <p>Context: {state}</p>
+      <button onClick={() => setState("Child 1 State")}>
+        Child 1 setState
+      </button>
+      <ChildOneNestedComponent />
+    </div>
+  );
+};
+
+const ChildTwoComponent = () => {
+  const { state, setState } = useContext(MyContext);
+
+  return (
+    <div style={style.container}>
+      <h3>Child 2</h3>
+      <p>Context: {state}</p>
+      <button onClick={() => setState("Child 2 State")}>
+        Child 2 setState
+      </button>
+    </div>
+  );
+};
+
+const ChildOneNestedComponent = () => {
+  const { state, setState } = useContext(MyContext);
+
+  return (
+    <div style={style.container}>
+      <h3>Child 1 Nested</h3>
+      <p>Context: {state}</p>
+      <button onClick={() => setState("Child 1 Nested State")}>
+        Child 1 Nested setState
+      </button>
+    </div>
+  );
+};
+
+export default function PropDrilling() {
+  return (
+    <>
+      <ParentComponent />
+    </>
+  );
+}
