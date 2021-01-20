a68d7e1cd476eae321839c7d8414d9b60e8629e0
vpvnguyen <vpvnguyen@gmail.com>
2021-01-19 20:22:07 -0800

    create and use login form context

--- src/components/UseContext/VNguyenV2/LoginV2.js
+++ src/components/UseContext/VNguyenV2/LoginV2.js
@@ -1,32 +1,64 @@
-import React from "react";
-import { useAuthenticationContext } from "./context/AuthenticationContextV2";
+import React, { useState } from "react";
+import {
+  useAuthenticationContext,
+  useAuthenticationValueContext,
+} from "./context/AuthenticationContextV2";
+import {
+  LoginFormContext,
+  useLoginFormContext,
+} from "./context/LoginFormContext";
 
-export const LoginButtonsV2 = () => {
+const LoginButtons = () => {
   const { dispatchAuthentication } = useAuthenticationContext();
+  const { name, setName } = useLoginFormContext(LoginFormContext);
 
-  const login = () => {
-    dispatchAuthentication({ type: "LOGIN" });
+  const handleLogin = (name) => {
+    dispatchAuthentication({ type: "LOGIN", payload: name });
   };
 
-  const logout = () => {
+  const handleLogout = () => {
     dispatchAuthentication({ type: "LOGOUT" });
+    setName("");
   };
 
   return (
     <>
-      <button onClick={() => login()}>Log In</button>
-      <button onClick={() => logout()}>Log Out</button>
+      <button onClick={() => handleLogin(name)}>Log In</button>
+      <button onClick={() => handleLogout()}>Log Out</button>
     </>
   );
 };
 
-export const LoginStateV2 = () => {
-  const { authenticationState } = useAuthenticationContext();
+const LoginState = () => {
+  const authenticationValue = useAuthenticationValueContext();
 
   return (
     <div>
-      Authentication State:{" "}
-      <b>{authenticationState ? "Logged In" : "Logged Out"}</b>
+      Authentication State: <b>{authenticationValue}</b>
     </div>
+  );
+};
+
+export const LoginForm = () => {
+  const [name, setName] = useState("");
+  const loginFormContextValue = { name, setName };
+
+  const handleOnChange = (event) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <LoginFormContext.Provider value={loginFormContextValue}>
+      <LoginState />
+      <h4>Login Form</h4>
+      <input
+        type="text"
+        label="name"
+        name="name"
+        value={name}
+        onChange={handleOnChange}
+      />
+      <LoginButtons />
+    </LoginFormContext.Provider>
   );
 };
