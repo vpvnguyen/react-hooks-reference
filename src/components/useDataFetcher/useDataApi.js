f12e8c05c898b725396bd3bc1abe3eb3d96cc96f
vpvnguyen <vpvnguyen@gmail.com>
2021-02-09 23:44:28 -0800

    create custom useDataApi hook

---
+++ src/components/useDataFetcher/useDataApi.js
@@ -0,0 +1,60 @@
+// SRC: https://www.robinwieruch.de/react-hooks-fetch-data
+
+import { useState, useEffect, useReducer } from "react";
+import axios from "axios";
+
+const FETCH_INIT = "FETCH_INIT";
+const FETCH_SUCCESS = "FETCH_SUCCESS";
+const FETCH_FAILURE = "FETCH_FAILURE";
+
+const dataFetchReducer = (state, action) => {
+  switch (action.type) {
+    case FETCH_INIT:
+      return { ...state, loading: true, error: false };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        error: false,
+        payload: action.payload,
+      };
+    case FETCH_FAILURE:
+      return { ...state, loading: false, error: true };
+    default:
+      throw new Error("dataFetchReducer error: state, action", state, action);
+  }
+};
+
+export const useDataApi = (initialUrl, initialData) => {
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    loading: false,
+    error: false,
+    data: initialData,
+  });
+
+  useEffect(() => {
+    let runTask = true;
+
+    const fetchData = async () => {
+      dispatch({ type: FETCH_INIT });
+
+      try {
+        const response = await axios(url);
+        if (runTask) dispatch({ type: FETCH_SUCCESS, payload: response.data });
+      } catch (error) {
+        console.error("fetchData error:", error);
+        if (runTask) dispatch({ type: FETCH_FAILURE });
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      runTask = false;
+    };
+  }, [url]);
+
+  return { loading, error, data, setUrl };
+};
--- src/components/useDataFetcher/useReducer.js
+++
@@ -1,3 +0,0 @@
-// SRC: https://www.robinwieruch.de/react-hooks-fetch-data
-
-import { useState, useEffect, useReducer } from "react";
