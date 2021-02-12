75f065f59af81300365435ad17ffacd8a60228c8
vpvnguyen <vpvnguyen@gmail.com>
2021-02-12 15:54:46 -0800

    destructure useDataFetcher object to use reducer's state

--- src/components/useDataFetcher/UserList.js
+++ src/components/useDataFetcher/UserList.js
@@ -15,27 +15,18 @@ const randomUserApiUrl = `https://randomuser.me/api/?results=5`;
 const initialData = null;
 
 export const UserList = () => {
-  const [{ loading, error, data }, setUrl] = useDataFetcher(randomUserApiUrl);
-
-  const handleOnClick = async (event) => {
-    await setUrl(randomUserApiUrl);
-
-    // event.preventDefault();
-    console.log(data);
-  };
+  const { loading, error, data, setUrl } = useDataFetcher(
+    randomUserApiUrl,
+    initialData
+  );
 
   return (
     <div style={style.container}>
-      <button onClick={(event) => handleOnClick(event)}>Get Users List</button>
-
       {loading && <div>Loading user list...</div>}
 
       {error && <div>There was an error loading the user list.</div>}
 
-      {data &&
-        data.results.map((user, index) => {
-          <div>{user.name.first}</div>;
-        })}
+      {data && data.results.map((user, index) => <div>{user.name.first}</div>)}
     </div>
   );
 };
