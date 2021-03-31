bf38a7d3a505016b44cd14c095e1e99b616e20f8
vpvnguyen <vpvnguyen@gmail.com>
2021-03-31 15:49:50 -0700

    updated with Kent C Dodd's new useContext example

---
+++ src/components/UseContext/KentCDoddsV2/count-context.js
@@ -0,0 +1,36 @@
+import React, { createContext, useReducer, useContext } from "react";
+
+const CountContext = createContext();
+
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case "increment": {
+      return { count: state.count + 1 };
+    }
+    case "decrement": {
+      return { count: state.count - 1 };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+};
+
+const CountProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(countReducer, { count: 0 });
+
+  // NOTE: might need to memoize this value
+  // http://kcd.im/optimize-context
+  const value = { state, dispatch };
+  return (
+    <CountContext.Provider value={value}>{children}</CountContext.Provider>
+  );
+};
+
+const useCount = () => {
+  const context = useContext(CountContext);
+  if (context === undefined) {
+    throw new Error("useCount must be used within a CountProvider");
+  }
+  return context;
+};
